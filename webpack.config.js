ae442c14dc947c9aff03bd5e9ca524ce56156aa3
Shota Hoshino <sht.hshn@gmail.com>
2016-06-28 22:22:34 +0900

    Added build script

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
+
+module.exports = {
+  debug: false,
+  devtool: 'source-map',
+  entry: 'index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'angular-provide.js',
+    sourceMapFilename: 'angular-provide.js.map'
+  },
+  resolve: {
+    extensions: ['', '.ts', '.js'],
+    root: path.resolve(__dirname, './src'),
+    modulesDirectories: ['node_modules']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'awesome-typescript-loader' }
+    ]
+  },
+  preLoaders: [
+    { test: /\.js$/, loader: 'source-map-loader' }
+  ],
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: { screw_ie8 : true },
+      compress: { screw_ie8: true },
+      comments: false
+    })
+  ]
+};
