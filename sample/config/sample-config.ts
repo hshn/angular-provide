eda3a91f5a5d7bc9149c7ee692924ac8712fef02
Shota Hoshino <sht.hshn@gmail.com>
2016-07-01 21:23:53 +0900

    Fix sample codes

--- sample/config/sample-config.ts
+++
@@ -1,8 +0,0 @@
-import ICompileProvider = angular.ICompileProvider;
-
-sampleProvider.$inject = ['$compileProvider'];
-export function sampleProvider($compileProvider: ICompileProvider) {
-  $compileProvider.component('foo', {
-    template: 'foo component',
-  });
-}
