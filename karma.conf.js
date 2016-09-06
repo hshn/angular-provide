b22a917db63a3c73f0f56937d746f4d5fe0dd743
Shota Hoshino <sht.hshn@gmail.com>
2016-09-06 21:14:45 +0900

    Tweak build configurations

--- karma.conf.js
+++ karma.conf.js
@@ -40,6 +40,10 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['mocha'],
 
+    mochaReporter: {
+      output: 'minimal'
+    },
+
 
     // web server port
     port: 9876,
